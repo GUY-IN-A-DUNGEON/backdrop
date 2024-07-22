56745541c8af3362187e576473a6ae4d1e13b259
GUY-IN-A-DUNGEON <118780058+GUY-IN-A-DUNGEON@users.noreply.github.com>
2024-07-21 23:43:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,9 +7,12 @@ let dotColor;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
-  // Create random dots
+  // Create random dots with initial velocities
   for (let i = 0; i < numDots; i++) {
-    let dot = createVector(random(width), random(height));
+    let dot = {
+      position: createVector(random(width), random(height)),
+      velocity: p5.Vector.random2D().mult(random(1, 2)) // Random velocity magnitude between 1 and 2
+    };
     dots.push(dot);
   }
   
@@ -22,14 +25,25 @@ function draw() {
   // Adjust line opacity based on mouse proximity
   lineOpacity = map(dist(mouseX, mouseY, width/2, height/2), 0, width/2, 100, 200);
   
-  // Draw lines between dots based on mouse proximity
+  // Update and draw lines between dots based on mouse proximity
   for (let i = 0; i < dots.length; i++) {
     for (let j = i + 1; j < dots.length; j++) {
-      let d = dist(dots[i].x, dots[i].y, dots[j].x, dots[j].y);
+      let d = dist(dots[i].position.x, dots[i].position.y, dots[j].position.x, dots[j].position.y);
       if (d < maxDist) {
         stroke(0, lineOpacity);
-        line(dots[i].x, dots[i].y, dots[j].x, dots[j].y);
+        line(dots[i].position.x, dots[i].position.y, dots[j].position.x, dots[j].position.y);
       }
+    }
+    
+    // Update dot position based on velocity
+    dots[i].position.add(dots[i].velocity);
+    
+    // Bounce off walls (simple edge detection)
+    if (dots[i].position.x < 0 || dots[i].position.x > width) {
+      dots[i].velocity.x *= -1;
+    }
+    if (dots[i].position.y < 0 || dots[i].position.y > height) {
+      dots[i].velocity.y *= -1;
     }
   }
   
@@ -41,6 +55,6 @@ function draw() {
   noStroke();
   fill(dotColor);
   for (let i = 0; i < dots.length; i++) {
-    ellipse(dots[i].x, dots[i].y, 5, 5);
+    ellipse(dots[i].position.x, dots[i].position.y, 5, 5);
   }
 }
